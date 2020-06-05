a7b8313e1be8d68e469bee9463dd373664175130
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-05 11:53:59 +0100

    This is the last of my editing on the first step of quote-generator.js

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,20 +1,16 @@
-
-const quotes = prompt("what is your quotes?: ");
+// created three different variable to store quotes
 var quotesA = [
-    ["The more you learn, the more you know something."], 
+    ["The more you learn the more you know something."], 
     ["Picking a piece from clump"], 
-    ["Hi there"], 
-    ["who is gona"], 
-    ["I will be there"]
-];
-
+    ["Hi there!"], 
+    ["who is gonna"], 
+    ["I will be there."]];
 var quotesB = [
     ["she has done it for a long time"],
     ["bring them here"],
-    ["you are so fast,"],
+    ["you are so fast!"],
     ["for everything"],
-    ["just a moment before you arrived"]
-];
+    ["just a moment before you arrived"]];
 
 var quotesC = [
     ["valuable thing?"],
@@ -24,8 +20,16 @@ var quotesC = [
     ["leave the town!"]
 ];
 
-var quotesGenerator = quotesA[0]+ " " + quotesB[3] +" " + quotesC[2];
-console.log(quotesGenerator);
+
+
+//then the quotes are generated randomly here.
+
+    let quoteIndex1 = Math.floor(Math.random() * quotesA.length);
+    let quoteIndex2 = Math.floor(Math.random() * quotesB.length);
+    let quoteIndex3 = Math.floor(Math.random() * quotesC.length);
+
+currentQuotes = quotesA[quoteIndex1] + " " + quotesB[quoteIndex2] + " " + quotesC[quoteIndex3];
+console.log(currentQuotes);
 
 
 
