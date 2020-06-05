7b740a892fee8a4d668fb3250c87caa5a43bc31f
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-05 08:24:33 +0100

    edited the quoteGeneration.js

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,24 +1,31 @@
-// // Here I created a variable called 'quotes' to store all of my arrays within strings.
 
+const quotes = prompt("what is your quotes?: ");
+var quotesA = [
+    ["The more you learn, the more you know something."], 
+    ["Picking a piece from clump"], 
+    ["Hi there"], 
+    ["who is gona"], 
+    ["I will be there"]
+];
 
-const numberOfQuote = prompt("Enter arrays of quotes: ");
- let quotes = [
-     ['I\'m the girl, who is always there for people when they need a friend. I\'m also the girl who faces many issues alone, but will still do anything to see someone else smile.-Ria shrivastava'],
-     ['What you wear and what you watch will control what you worship. -Brocc Chavis'],
-     ['Young people need to understand that they can do the same thing, they are not powerless, that they actually can make a change. -Cameron Boyce'],
-     ['If you want to follow jesus because He will give you a better life, that is idolatry. Follow Christ for the sake of Christ, He is worthy. -Paul Washer'],
-     ['Don\'t waste words on people who deserve your silence. Sometimes the most powerful thing you ca say is nothing at all. -Mandy Hale']
- ];
-    numOfQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    console.log(quotes);
-    function getQuote() {
-     currentQuote = quotes[[2][3]];
-    console.log(getQuote);
-    }
+var quotesB = [
+    ["she has done it for a long time"],
+    ["bring them here"],
+    ["you are so fast,"],
+    ["for everything"],
+    ["just a moment before you arrived"]
+];
 
-// I shoul have offer the quote to generate new quote if I still had time.....
+var quotesC = [
+    ["valuable thing?"],
+    ["that's nice to hear!"],
+    ["I don't want to be the last."],
+    ["she dresses well."],
+    ["leave the town!"]
+];
 
-
+var quotesGenerator = quotesA[0]+ " " + quotesB[3] +" " + quotesC[2];
+console.log(quotesGenerator);
 
 
 
